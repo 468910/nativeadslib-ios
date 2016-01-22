8b4f4ea9d6617cf8748a4f14ac32a319b41c5bc5
Adrián Moreno Peña <adrian@pocketmedia.mobi>
2016-01-22 14:25:06 +0100

    Library updated.

--- Example/Pods/Target Support Files/PocketMediaNativeAds/PocketMediaNativeAds-umbrella.h
+++ Example/Pods/Target Support Files/PocketMediaNativeAds/PocketMediaNativeAds-umbrella.h
@@ -1,6 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "PocketMediaNativeAds-PocketMediaNativeAds-Bridging-Header.h"
 
 FOUNDATION_EXPORT double PocketMediaNativeAdsVersionNumber;
 FOUNDATION_EXPORT const unsigned char PocketMediaNativeAdsVersionString[];
