630205924585b4212c2d3d1300adf9d24ea9b348
Kees Bank <apple@MacBook-Pro-di-apple.local>
2016-07-04 10:49:03 +0200

    Added ScrollView Button

--- Example/PocketMediaNativeAds/MenuViewController.swift
+++ Example/PocketMediaNativeAds/MenuViewController.swift
@@ -13,14 +13,15 @@ class MenuViewController : UIViewController, UIPopoverPresentationControllerDele
   
   @IBOutlet weak var collectionViewAdsButton: UIButton!
   @IBOutlet weak var tableViewAdsButton: UIButton!
+  @IBOutlet weak var scrollViewButton: UIButton!
   @IBOutlet weak var pocketMediaLogo: UIImageView!
-  
   
   
   override func viewDidLoad() {
     let nav = self.navigationController?.navigationBar
     nav?.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
     
+    //CollectionViewButton
     collectionViewAdsButton.titleEdgeInsets.left = 10
     collectionViewAdsButton.titleEdgeInsets.right = 10
     collectionViewAdsButton.titleLabel?.textColor = UIColor.whiteColor()
@@ -29,13 +30,22 @@ class MenuViewController : UIViewController, UIPopoverPresentationControllerDele
     
     
     
-    
+    // TableViewButton
     tableViewAdsButton.layer.cornerRadius = 10
     tableViewAdsButton.backgroundColor = UIColor(red: 11 / 255, green: 148 / 255, blue: 68 / 255,  alpha: 1)
     
     tableViewAdsButton.titleEdgeInsets.left = 10
     tableViewAdsButton.titleEdgeInsets.right = 10
     tableViewAdsButton.titleLabel?.textColor = UIColor.whiteColor()
+   
+    // ScrollVieWButton
+    scrollViewButton.setTitle("ScrollView", forState: UIControlState.Normal)
+    scrollViewButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+    scrollViewButton.titleEdgeInsets.left = 10
+    scrollViewButton.titleEdgeInsets.right = 10
+    scrollViewButton.layer.cornerRadius = 10
+    scrollViewButton.backgroundColor = UIColor(red: 11 / 255, green: 148 / 255, blue: 68 / 255, alpha: 1)
+    scrollViewButton.titleLabel?.textColor = UIColor.whiteColor()
     
     pocketMediaLogo.contentMode = .ScaleAspectFit
     
