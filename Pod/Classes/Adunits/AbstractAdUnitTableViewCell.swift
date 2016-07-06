340eba3dc5d528e311c97d622456de014007f5ef
Kees Bank <apple@MacBook-Pro-di-apple.local>
2016-07-06 11:39:30 +0200

    Work in progress save state

--- Pod/Classes/Adunits/AbstractAdUnitTableViewCell.swift
+++ Pod/Classes/Adunits/AbstractAdUnitTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AlamofireImage
 import Haneke
+import Darwin
 
 public class AbstractAdUnitTableViewCell : UITableViewCell, NativeAdViewBinderProtocol {
   
@@ -30,7 +31,7 @@ public class AbstractAdUnitTableViewCell : UITableViewCell, NativeAdViewBinderPr
   }
   
   
-  
+  // After has been loaded from Nib
   public override func awakeFromNib() {
       super.awakeFromNib()
       adDescription.numberOfLines = 0
@@ -41,10 +42,73 @@ public class AbstractAdUnitTableViewCell : UITableViewCell, NativeAdViewBinderPr
       adTitle.lineBreakMode = .ByTruncatingTail
       adDescription.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width * 0.70
     
+    
+      // Setting AdDescription And Adtitle
+    
+
+      self.adDescription.backgroundColor = UIColor.redColor()
+      self.adTitle.backgroundColor = UIColor.yellowColor()
+    
+  
+  
+    
+  }
+ 
+  // Used to change subviews
+  public override func layoutSubviews() {
+  
+    print("I'M invoked")
+    
+    
+    
+    /*if(self.adDescription.frame.maxY > self.adImage.frame.maxY){
+      var point = CGPoint(x: adDescription.frame.origin.x, y: adImage.frame.maxY - adDescription.frame.origin.y)
+      self.adDescription.frame = CGRectMake(point.x, point.y, adDescription.frame.width,
+                                            abs(adImage.frame.maxY - adDescription.frame.origin.y))
+        centerAlignY.active = false
+    }*/
+  
+  }
+  
+  
+  
+  public override func updateConstraints() {
+    super.updateConstraints()
+    print("Current addescription height")
+    /**
+    if(self.adDescription.frame.height > 16){
+      centerAlignY = MyConstraint.changeMultiplier(centerAlignY, multiplier: 0.5)
+    }else {
+      centerAlignY = MyConstraint.changeMultiplier(centerAlignY, multiplier: 1)
+    }*/
+    
+   
+      
+
+    
   }
   
 }
 
+struct MyConstraint {
+  static func changeMultiplier(constraint: NSLayoutConstraint, multiplier: CGFloat) -> NSLayoutConstraint {
+    let newConstraint = NSLayoutConstraint(
+      item: constraint.firstItem,
+      attribute: constraint.firstAttribute,
+      relatedBy: constraint.relation,
+      toItem: constraint.secondItem,
+      attribute: constraint.secondAttribute,
+      multiplier: multiplier,
+      constant: constraint.constant)
+    
+    newConstraint.priority = constraint.priority
+    
+    NSLayoutConstraint.deactivateConstraints([constraint])
+    NSLayoutConstraint.activateConstraints([newConstraint])
+    
+    return newConstraint
+  }
+}
 
 
 extension UIImageView {
