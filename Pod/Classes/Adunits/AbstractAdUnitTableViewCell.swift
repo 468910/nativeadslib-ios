1d74e2779baf73fe4e985ac9bbbf4370ae01a1b3
Adrián Moreno Peña <adrian@pocketmedia.mobi>
2016-07-27 12:09:23 +0200

    Code formatting with Swimat.

--- Pod/Classes/Adunits/AbstractAdUnitTableViewCell.swift
+++ Pod/Classes/Adunits/AbstractAdUnitTableViewCell.swift
@@ -10,107 +10,90 @@ import UIKit
 import Haneke
 import Darwin
 
-
 /**
  Class to be subclassed for use with the AdStream.
  **/
-public class AbstractAdUnitTableViewCell : UITableViewCell, NativeAdViewBinderProtocol {
-  
-  @IBOutlet weak var speakerPhone : UIImageView!
-  @IBOutlet weak var adImage : UIImageView!
-  @IBOutlet weak var adTitle: UILabel!
-  @IBOutlet weak var adDescription : UILabel!
-  @IBOutlet weak var adInfoView: UIView!
-  
-  @IBOutlet weak var adDescriptionHeightConstraint: NSLayoutConstraint!
-  @IBOutlet weak var middleLineCenterYConstraint: NSLayoutConstraint!
-  
-  
-  public func configureAdView(nativeAd: NativeAd) {
-    adTitle.text = nativeAd.campaignName
-    adDescription.text = nativeAd.campaignDescription
-    print(nativeAd.campaignImage)
-    adImage.hnk_setImageFromURL(nativeAd.campaignImage, placeholder: UIImage(), format: nil, failure: nil, success: nil)
-  }
-  
-  func configureAdView(nativeAd: NativeAd, viewController: UIViewController) {
-    abort()
-  }
-  
-  
-  // After has been loaded from Nib
-  public override func awakeFromNib() {
-      super.awakeFromNib()
-      adDescription.numberOfLines = 0
-      adDescription.lineBreakMode = .ByTruncatingTail
-      adDescription.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width * 0.80
-    
-      adTitle.numberOfLines = 0
-      adTitle.lineBreakMode = .ByTruncatingTail
-      adDescription.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width * 0.70
-    
-    
-      // Setting AdDescription And Adtitle
-    
+public class AbstractAdUnitTableViewCell: UITableViewCell, NativeAdViewBinderProtocol {
 
-      self.adDescription.backgroundColor = UIColor.redColor()
-      self.adTitle.backgroundColor = UIColor.yellowColor()
-    
-  
-  
-    
-  }
- 
-  // Used to change subviews
-  public override func layoutSubviews() {
-  
-    print("I'M invoked")
-  }
-  
-  
-  
-  public override func updateConstraints() {
-    super.updateConstraints()
-    
-    var lineCount = 0;
-    let textSize = CGSizeMake(adDescription.frame.size.width, CGFloat(Float.infinity));
-    let rHeight = lroundf(Float(adDescription.sizeThatFits(textSize).height))
-    let charSize = lroundf(Float(adDescription.font.lineHeight));
-    lineCount = rHeight/charSize
-    print("No of lines \(lineCount)")
-    
-    if(lineCount > 1){
-      
-    }
-    
-    
-    
-   
-      
+	@IBOutlet weak var speakerPhone: UIImageView!
+	@IBOutlet weak var adImage: UIImageView!
+	@IBOutlet weak var adTitle: UILabel!
+	@IBOutlet weak var adDescription: UILabel!
+	@IBOutlet weak var adInfoView: UIView!
 
-    
-  }
-  
+	@IBOutlet weak var adDescriptionHeightConstraint: NSLayoutConstraint!
+	@IBOutlet weak var middleLineCenterYConstraint: NSLayoutConstraint!
+
+	public func configureAdView(nativeAd: NativeAd) {
+		adTitle.text = nativeAd.campaignName
+		adDescription.text = nativeAd.campaignDescription
+		print(nativeAd.campaignImage)
+		adImage.hnk_setImageFromURL(nativeAd.campaignImage, placeholder: UIImage(), format: nil, failure: nil, success: nil)
+	}
+
+	func configureAdView(nativeAd: NativeAd, viewController: UIViewController) {
+		abort()
+	}
+
+	// After has been loaded from Nib
+	public override func awakeFromNib() {
+		super.awakeFromNib()
+		adDescription.numberOfLines = 0
+		adDescription.lineBreakMode = .ByTruncatingTail
+		adDescription.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width * 0.80
+
+		adTitle.numberOfLines = 0
+		adTitle.lineBreakMode = .ByTruncatingTail
+		adDescription.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width * 0.70
+
+		// Setting AdDescription And Adtitle
+
+		self.adDescription.backgroundColor = UIColor.redColor()
+		self.adTitle.backgroundColor = UIColor.yellowColor()
+
+	}
+
+	// Used to change subviews
+	public override func layoutSubviews() {
+
+		print("I'M invoked")
+	}
+
+	public override func updateConstraints() {
+		super.updateConstraints()
+
+		var lineCount = 0;
+		let textSize = CGSizeMake(adDescription.frame.size.width, CGFloat(Float.infinity));
+		let rHeight = lroundf(Float(adDescription.sizeThatFits(textSize).height))
+		let charSize = lroundf(Float(adDescription.font.lineHeight));
+		lineCount = rHeight / charSize
+		print("No of lines \(lineCount)")
+
+		if (lineCount > 1) {
+
+		}
+
+	}
+
 }
 
 struct MyConstraint {
-  static func changeMultiplier(constraint: NSLayoutConstraint, multiplier: CGFloat) -> NSLayoutConstraint {
-    let newConstraint = NSLayoutConstraint(
-      item: constraint.firstItem,
-      attribute: constraint.firstAttribute,
-      relatedBy: constraint.relation,
-      toItem: constraint.secondItem,
-      attribute: constraint.secondAttribute,
-      multiplier: multiplier,
-      constant: constraint.constant)
-    
-    newConstraint.priority = constraint.priority
-    
-    NSLayoutConstraint.deactivateConstraints([constraint])
-    NSLayoutConstraint.activateConstraints([newConstraint])
-    
-    return newConstraint
-  }
-}
+	static func changeMultiplier(constraint: NSLayoutConstraint, multiplier: CGFloat) -> NSLayoutConstraint {
+		let newConstraint = NSLayoutConstraint(
+			item: constraint.firstItem,
+			attribute: constraint.firstAttribute,
+			relatedBy: constraint.relation,
+			toItem: constraint.secondItem,
+			attribute: constraint.secondAttribute,
+			multiplier: multiplier,
+			constant: constraint.constant)
 
+		newConstraint.priority = constraint.priority
+
+		NSLayoutConstraint.deactivateConstraints([constraint])
+		NSLayoutConstraint.activateConstraints([newConstraint])
+
+		return newConstraint
+	}
+}
 
--- Pod/Classes/Core/NativeAdsRequest.swift
+++ Pod/Classes/Core/NativeAdsRequest.swift
@@ -12,129 +12,121 @@ import AdSupport
 /**
  Object which is used to make a NativeAdsRequest has to be used in combination with the NativeAdsConnectionDelegate
  */
-public class NativeAdsRequest : NSObject, NSURLConnectionDelegate, UIWebViewDelegate {
-    
-    /// Object to notify about the updates related with the ad request
-    public var delegate: NativeAdsConnectionDelegate?
-    /// To allow more verbose logging and behaviour
-    public var debugModeEnabled : Bool = false
-    /// Parameters private setter
-    private(set) var parameters : [String : String] = [
-    "token" : (ASIdentifierManager.sharedManager().advertisingIdentifier?.UUIDString)!,
-    "os" : "ios",
-    "version" : "\(NativeAdsConstants.Device.iosVersion)",
-    "model" : NativeAdsConstants.Device.model,
-   ]
-  
-   public enum imageType : String {
-    case all_images = ""
-    case icon = "icon"
-    case hq_icon = "hq_icon"
-    case banner = "banner"
-    case bigImages = "banner,hq_icon"
-    case bannerAndIcons = "banner,icon"
-   }
-  
-  public var imageFilter : imageType = .all_images {
-    willSet(newImageType){
-      NSLog("NativeAdsRequest", "Changing imagefilter")
-    }
-    didSet {
-      if imageFilter == imageType.all_images {
-        parameters.removeValueForKey("image_filter")
-      }else{
-      parameters["image_type"] = imageFilter.rawValue
-      }
-    }
-  }
-  
-  
-    public init(adPlacementToken : String?, delegate: NativeAdsConnectionDelegate?) {
-        super.init()
-      
-        parameters["placement_key"]  = adPlacementToken!
-      
-        if (!ASIdentifierManager.sharedManager().advertisingTrackingEnabled){
-          parameters["optout"] = "1"
-        }
-      
-      
-        self.delegate = delegate
-   }
-  
-    /**
-     Method used to retrieve native ads which are later accessed by using the delegate.
-     - limit: Limit on how many native ads are to be retrieved.
-     */
-    @objc
-    public func retrieveAds(limit: UInt){
-        
-      
-      if let url = NSURL.initWithParameters(self.parameters, path: NativeAdsConstants.NativeAds.baseUrl.rawValue){
-            var request = NSMutableURLRequest.init(URL: url)
-            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-                
-                if error != nil {
-                    
-                    self.delegate?.didRecieveError(error!)
-                    
-                } else {
-                    var nativeAds: [NativeAd] = []
-                    if let json: NSArray = (try? NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as? NSArray {
-                        
-                        json.filter({ ($0 as? NSDictionary) != nil}).forEach({ (element) -> () in
-                            if let ad = NativeAd( adDictionary: element as! NSDictionary, adPlacementToken: self.parameters["placement_key"]!){
-                                nativeAds.append(ad)
-                            }
-                            
-                        })
-                        
-                        if nativeAds.count > 0 {
-                            self.delegate?.didReceiveResults(nativeAds)
-                        } else {
-                            let userInfo = ["No ads available from server": NSLocalizedDescriptionKey]
-                            let error = NSError(domain: "mobi.pocketmedia.nativeads", code: -1, userInfo: userInfo)
-                            self.delegate?.didRecieveError(error)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
+public class NativeAdsRequest: NSObject, NSURLConnectionDelegate, UIWebViewDelegate {
+
+	/// Object to notify about the updates related with the ad request
+	public var delegate: NativeAdsConnectionDelegate?
+	/// To allow more verbose logging and behaviour
+	public var debugModeEnabled: Bool = false
+	/// Parameters private setter
+	private(set) var parameters: [String: String] = [
+		"token": (ASIdentifierManager.sharedManager().advertisingIdentifier?.UUIDString)!,
+		"os": "ios",
+		"version": "\(NativeAdsConstants.Device.iosVersion)",
+		"model": NativeAdsConstants.Device.model,
+	]
+
+	public enum imageType: String {
+		case all_images = ""
+		case icon = "icon"
+		case hq_icon = "hq_icon"
+		case banner = "banner"
+		case bigImages = "banner,hq_icon"
+		case bannerAndIcons = "banner,icon"
+	}
+
+	public var imageFilter: imageType = .all_images {
+		willSet(newImageType) {
+			NSLog("NativeAdsRequest", "Changing imagefilter")
+		}
+		didSet {
+			if imageFilter == imageType.all_images {
+				parameters.removeValueForKey("image_filter")
+			} else {
+				parameters["image_type"] = imageFilter.rawValue
+			}
+		}
+	}
+
+	public init(adPlacementToken: String?, delegate: NativeAdsConnectionDelegate?) {
+		super.init()
+
+		parameters["placement_key"] = adPlacementToken!
+
+		if (!ASIdentifierManager.sharedManager().advertisingTrackingEnabled) {
+			parameters["optout"] = "1"
+		}
+
+		self.delegate = delegate
+	}
+
+	/**
+	 Method used to retrieve native ads which are later accessed by using the delegate.
+	 - limit: Limit on how many native ads are to be retrieved.
+	 */
+	@objc
+	public func retrieveAds(limit: UInt) {
+
+		if let url = NSURL.initWithParameters(self.parameters, path: NativeAdsConstants.NativeAds.baseUrl.rawValue) {
+			var request = NSMutableURLRequest.init(URL: url)
+			NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) in
+
+				if error != nil {
+
+					self.delegate?.didRecieveError(error!)
+
+				} else {
+					var nativeAds: [NativeAd] = []
+					if let json: NSArray = (try? NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as? NSArray {
+
+						json.filter({ ($0 as? NSDictionary) != nil }).forEach({ (element) -> () in
+							if let ad = NativeAd(adDictionary: element as! NSDictionary, adPlacementToken: self.parameters["placement_key"]!) {
+								nativeAds.append(ad)
+							}
+
+						})
+
+						if nativeAds.count > 0 {
+							self.delegate?.didReceiveResults(nativeAds)
+						} else {
+							let userInfo = ["No ads available from server": NSLocalizedDescriptionKey]
+							let error = NSError(domain: "mobi.pocketmedia.nativeads", code: -1, userInfo: userInfo)
+							self.delegate?.didRecieveError(error)
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
-
-
-
 
 extension NSURL {
-  
-  static func initWithParameters(parameters: Dictionary<String, String> , path: String) -> NSURL? {
-    let parameterArray = parameters.map { (key, value) -> String in
-      return "\(key)=\(value.stringByAddingPercentEscapesForQueryValue()!)"
-    }
-    var string = "\(path)&\(parameterArray.joinWithSeparator("&"))"
-    print(string)
-    return NSURL(string: string)
-  }
-  
-  
+
+	static func initWithParameters(parameters: Dictionary<String, String>, path: String) -> NSURL? {
+		let parameterArray = parameters.map { (key, value) -> String in
+			return "\(key)=\(value.stringByAddingPercentEscapesForQueryValue()!)"
+		}
+		var string = "\(path)&\(parameterArray.joinWithSeparator("&"))"
+		print(string)
+		return NSURL(string: string)
+	}
+
 }
 
-
 extension String {
-  
-  /// Percent escape value to be added to a URL query value as specified in RFC 3986
-  ///
-  /// This percent-escapes all characters except the alphanumeric character set and "-", ".", "_", and "~".
-  ///
-  /// http://www.ietf.org/rfc/rfc3986.txt
-  ///
-  /// - returns:   Return precent escaped string.
-  
-  func stringByAddingPercentEscapesForQueryValue() -> String? {
-    let characterSet = NSMutableCharacterSet.alphanumericCharacterSet()
-    characterSet.addCharactersInString("-._~")
-    return stringByAddingPercentEncodingWithAllowedCharacters(characterSet)
-  }
+
+	/// Percent escape value to be added to a URL query value as specified in RFC 3986
+	///
+	/// This percent-escapes all characters except the alphanumeric character set and "-", ".", "_", and "~".
+	///
+	/// http://www.ietf.org/rfc/rfc3986.txt
+	///
+	/// - returns:   Return precent escaped string.
+
+	func stringByAddingPercentEscapesForQueryValue() -> String? {
+		let characterSet = NSMutableCharacterSet.alphanumericCharacterSet()
+		characterSet.addCharactersInString("-._~")
+		return stringByAddingPercentEncodingWithAllowedCharacters(characterSet)
+	}
 }
