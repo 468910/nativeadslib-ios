b0e74561161482a7e87108f63a29430fcdbe2b47
Iain Munro <iain17@me.com>
2016-09-08 18:49:21 +0200

    More travis drama.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 set -e
-set -o pipefail && xcodebuild -verbose -workspace PocketMediaNativeAds.xcworkspace -scheme "PocketMediaNativeAdsTests" clean
-sleep 2
-set -o pipefail && xcodebuild -verbose -workspace PocketMediaNativeAds.xcworkspace -scheme "PocketMediaNativeAdsTests" -destination "platform=iOS Simulator,name=iPhone 6" test
+set -o pipefail && xcodebuild -verbose -workspace PocketMediaNativeAds.xcworkspace -scheme "PocketMediaNativeAdsTests" -destination "platform=iOS Simulator,name=iPhone 6" clean test
