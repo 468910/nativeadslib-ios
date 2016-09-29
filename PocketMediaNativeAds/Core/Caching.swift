0a08bd95871a1412707ba79a69e56090e03e98db
Kees Bank <kees@pocketmedia.mobi>
2016-09-29 14:33:17 +0200

    renamed nativeSetImageFromUrl to remove ambiguity, fixed row height

--- PocketMediaNativeAds/Core/Caching.swift
+++ PocketMediaNativeAds/Core/Caching.swift
@@ -56,7 +56,7 @@ extension NSURL {
 }
 
 extension UIImageView {
-    func setImageFromURL(url: NSURL) {
+    func nativeSetImageFromURL(url: NSURL) {
         //self.image = drawCustomImage(CGSize(width: 100, height: 100))
         if let campaignImage = url.cachedImage {
             //Cached
