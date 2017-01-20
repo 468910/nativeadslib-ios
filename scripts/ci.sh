c4298129f2c90735df02830c3b418d52981b258e
Iain Munro <iain17@me.com>
2017-01-20 17:17:15 +0100

    No umbrella pls.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Run this from the project root.
-xcodebuild -verbose -workspace PocketMediaNativeAds.xcworkspace -scheme "PocketMediaNativeAds Library - Unit tests" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone Retina (4-inch)' test
+xcodebuild -verbose -workspace PocketMediaNativeAds.xcworkspace -scheme "PocketMediaNativeAds Library - Unit tests" -sdk iphonesimulator -destination 'platform=iOS Simulator,id=iPhone 6s' test
